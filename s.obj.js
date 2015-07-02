f0ee392f1a1ad6022842f7803f312023698fc914
Mike Acton <macton@gmail.com>
2015-07-02 10:24:54 -0700

    initial add

---
+++ s.obj.js
@@ -0,0 +1,39 @@
+var S = require('s');
+
+var obj = {
+  keys: function( obj ) {
+    return Object.keys( obj );
+  },
+  create: function( obj ) {
+    return Object.create( Object.prototype, obj );
+  },
+  fromList: S.curry( function( key, value, list ) {
+    return S.list.reduce( function( obj, element ) {
+      obj[ S.cv( key, element ) ] = S.cv( value, element );
+      return obj;
+    }, {}, list );
+  }),
+  max: S.curry( function( fn, obj ) {
+    return S.obj.keys( obj ).reduce( function( max, key ) {
+      var value = S.cv( fn, obj[key] ) ;
+      return value > max ? value : max;
+    }, 0);
+  }),
+  def: {
+    lazy: function(fn) {
+      var value;
+      return {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+          if (value === void 0) {
+            value = fn.call(this);
+          }
+          return value;
+        }
+      };
+    },
+  },
+};
+
+exports = module.exports = obj;
---
+++ test/test.js
@@ -0,0 +1,5 @@
+var S = require('s');
+
+S.obj = require('../s.obj');
+
+console.log( S.obj.keys( { Foo: 'foo', Bar: 'bar' } ) );

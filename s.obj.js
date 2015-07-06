e9f3c32ca8ceff9316ff82ec7b96c45c1534abed
Mike Acton <macton@gmail.com>
2015-07-06 13:53:00 -0700

    remove dependencies

--- s.obj.js
+++ s.obj.js
@@ -8,13 +8,13 @@ var obj = {
     return Object.create( Object.prototype, obj );
   },
   fromList: S.curry( function( key, value, list ) {
-    return S.list.reduce( function( obj, element ) {
+    return list.reduce( function( obj, element ) {
       obj[ S.cv( key, element ) ] = S.cv( value, element );
       return obj;
-    }, {}, list );
+    }, {} );
   }),
   max: S.curry( function( fn, obj ) {
-    return S.obj.keys( obj ).reduce( function( max, key ) {
+    return Object.keys( obj ).reduce( function( max, key ) {
       var value = S.cv( fn, obj[key] ) ;
       return value > max ? value : max;
     }, 0);
